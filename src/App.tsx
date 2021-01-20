dfc1953f4abe204f17c34644bdbf29ee31657cc4
jhamPac <cloud@kevia.me>
2021-01-19 17:35:55 -0700

    Remove the rest of CRA files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import * as React from 'react'
 
-function App() {
+export default function App(): React.ReactElement {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nothing</h1>
     </div>
   );
 }
-
-export default App;
